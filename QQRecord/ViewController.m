60160448b7fdc11a9fa7d36401decb3790f78593
pencilCool <yhtangcoder@gmail.com>
2017-06-24 07:11:11 +0800

    init

--- QQRecord/ViewController.m
+++ QQRecord/ViewController.m
@@ -7,6 +7,16 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
+#import <AVKit/AVKit.h>
+
+@interface ViewController()
+@property (weak) IBOutlet NSScrollView *persentationView;
+@property (nonatomic, strong) AVAudioPlayer *audioPlay;
+@property (nonatomic, strong) AVAudioRecorder *audioRecorder;
+@end
+
+
 
 @implementation ViewController
 
@@ -14,6 +24,29 @@
     [super viewDidLoad];
 
     // Do any additional setup after loading the view.
+    
+    NSDictionary *recordSetting = [NSDictionary dictionaryWithObjectsAndKeys:
+                                   [NSNumber numberWithInt:AVAudioQualityLow],AVEncoderAudioQualityKey,
+                                   [NSNumber numberWithInt:16],AVEncoderBitRateKey,
+                                   [NSNumber numberWithInt:2],AVNumberOfChannelsKey,
+                                   [NSNumber numberWithFloat:44100.0],AVSampleRateKey,
+                                   nil];
+   // NSURL *url =  NSFileManager.defaultManager.homeDirectoryForCurrentUser;
+    NSURL *url = [NSURL fileURLWithPath:[NSString stringWithFormat:@"%@/record.caf", [[NSBundle mainBundle] resourcePath]]];
+    
+    // url 打个断点看看路劲 比如：file:///Users/pencilcool/Library/Developer/Xcode/DerivedData/QQRecord-etwkrufqwxhhbubptygagennpfnq/Build/Products/Debug/QQRecord.app/Contents/Resources/record
+    
+    
+    NSError *error = nil;
+    self.audioRecorder = [[ AVAudioRecorder alloc] initWithURL:url settings:recordSetting error:&error];
+    if (error != nil) {
+        NSLog(@"Init audioRecorder error: %@",error);
+    }else{
+        if ([self.audioRecorder prepareToRecord]) {
+            NSLog(@"Prepare successful");
+        }
+    }
+    
 }
 
 
@@ -22,6 +55,26 @@
 
     // Update the view, if already loaded.
 }
+- (IBAction)start:(id)sender {
+    if (!self.audioRecorder.recording) {
+        NSLog(@"start-------");
+        [self.audioRecorder record];
+    }
+    
+}
+
+
+- (IBAction)stop:(id)sender {
+    if (self.audioRecorder.recording) {
+        NSLog(@"stop-------");
+        [self.audioRecorder stop];
+    }
+}
+
+- (void)updateWith:(NSString *) str {
+    
+}
+
 
 
 @end
